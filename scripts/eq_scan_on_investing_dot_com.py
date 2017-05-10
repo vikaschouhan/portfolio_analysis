b6ed5cfcbe232a0c2464f4db0ed09deafff2c959
vikaschouhan <presentisgood@gmail.com>
2017-05-10 20:02:19 +0530

    Changing author.

--- scripts/eq_scan_on_investing_dot_com.py
+++ scripts/eq_scan_on_investing_dot_com.py
@@ -70,6 +70,50 @@ def unixdate_now():
 def strdate_now():
     return datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S")
 
+# BSE bhavcopy data
+def bse_latest_bhavcopy_info():
+    l_file = None
+    date_y   = datetime.datetime.today() - datetime.timedelta(days=1)    # yesterday date
+    shift    = datetime.timedelta(max(1,(date_y.weekday() + 6) % 7 - 3))
+    date_y   = date_y - shift
+    url_this = "http://www.bseindia.com/download/BhavCopy/Equity/EQ{:02d}{:02d}{:02d}_CSV.ZIP".format(date_y.day, date_y.month, date_y.year % 2000)
+    print "Fetching BSE Bhavcopy from {}".format(url_this)
+    d_data   = urllib.urlopen(url_this)
+    l_file   = StringIO(d_data.read())
+    
+    # Read zip file
+    zip_f    = zipfile.ZipFile(l_file)
+    csv_f    = csv.reader(zip_f.open(zip_f.namelist()[0]))
+    bse_dict = {}
+    ctr      = 0
+
+    for item_this in csv_f:
+        # Convert strings to integers/floats
+        if ctr != 0:
+            cc_dict = {
+                "bse_code"        : int(item_this[0]),
+                "bse_name"        : item_this[1].rstrip(),
+                "bse_group"       : item_this[2].rstrip(),
+                "bse_type"        : item_this[3].rstrip(),
+                "open"            : float(item_this[4]),
+                "high"            : float(item_this[5]),
+                "low"             : float(item_this[6]),
+                "close"           : float(item_this[7]),
+                "last"            : float(item_this[8]),
+                "prev_close"      : float(item_this[9]),
+                "no_trades"       : int(item_this[10]),
+                "no_shares"       : int(item_this[11]),
+                "net_turnover"    : float(item_this[12]),
+            }
+            bse_dict[cc_dict['bse_code']] = cc_dict
+            #db.screener_in.update({ 'bse_code' : cc_dict['bse_code'] }, { '$set' : { 'bse_bhavcopy' : cc_dict } })
+        # endif
+        ctr = ctr + 1
+    # endfor
+    return bse_dict
+# enddef
+
+
 # NSE bhavcopy data
 def nse_latest_bhavcopy_info():
     l_file = None
@@ -107,15 +151,19 @@ def nse_latest_bhavcopy_info():
 # enddef
 
 # Scan for securities
-def scan_securities(name):
-    exchange = 'NS'
+def scan_securities(name, exchange):
+    if exchange not in ['NS', 'BO']:
+        print "Exchange could only be {}".format(['NS', 'BO'])
+        sys.exit(-1)
+    # endif
     this_url = g_bsurl(sock) + "query={}&type=Stock&exchange={}".format(name, exchange)
 
     #print "{} : Fetching {}".format(strdate_now(), this_url)
     response = urllib.urlopen(this_url)
     j_data   = json.loads(response.read())
     if not bool(j_data):
-        print "{} : Not able to fetch.".format(strdate_now())
+        #print "\n{} : Not able to fetch.".format(strdate_now())
+        pass
     # endif
     return j_data
 # enddef
@@ -128,25 +176,50 @@ if __name__ == '__main__':
 
     invs_l   = []
     output_f = os.path.expandvars('$HOME') + '/investing_dot_com_security_dict.py'
-    nse_bvc  = nse_latest_bhavcopy_info()
-    nse_cl   = nse_bvc.keys()
-    nse_cn   = len(nse_cl)
-    cn_indx  = 1
+    fetch_nse = True
+    fetch_bse = True
 
     # get socket
     sock = g_sock()
     print "sock = {}".format(sock)
 
-    # Iterate over all keys
-    for sec_name in nse_cl:
-        sec_data = scan_securities(sec_name)
-        if len(sec_data) > 0:
-            invs_l.append(scan_securities(sec_name)[0])
-        # endif
-        sys.stdout.write('\r>> Querying..  {}/{}'.format(cn_indx, nse_cn))
-        sys.stdout.flush()
-        cn_indx = cn_indx + 1
-    # endfor
+    # Iterate over all keys of nse
+    if fetch_nse:
+        cn_indx = 1
+        fn_indx = 0
+        nse_bvc = nse_latest_bhavcopy_info()
+        nse_cl  = nse_bvc.keys()
+        nse_cn  = len(nse_cl)
+        for sec_name in nse_cl:
+            sec_data = scan_securities(sec_name, 'NS')
+            if len(sec_data) > 0:
+                invs_l.append(sec_data[0])
+                fn_indx = fn_indx + 1
+            # endif
+            sys.stdout.write('\r>> Querying NSE ..  {}/{}/{}'.format(cn_indx, nse_cn, fn_indx))
+            sys.stdout.flush()
+            cn_indx = cn_indx + 1
+        # endfor
+    # endif
+
+    # Iterate over all keys of bse
+    if fetch_bse:
+        cn_indx = 1
+        fn_indx = 0
+        bse_bvc = bse_latest_bhavcopy_info()
+        bse_cl  = bse_bvc.keys()
+        bse_cn  = len(bse_cl)
+        for sec_code in bse_cl:
+            sec_data = scan_securities(sec_code, 'BO')
+            if len(sec_data) > 0:
+                invs_l.append(sec_data[0])
+                fn_indx = fn_indx + 1
+            # endif
+            sys.stdout.write('\r>> Querying BSE ..  {}/{}/{}'.format(cn_indx, bse_cn, fn_indx))
+            sys.stdout.flush()
+            cn_indx = cn_indx + 1
+        # endfor
+    # endif
 
     print "Done !!"
 
