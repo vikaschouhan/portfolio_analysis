09174855041a690677291b43dbefa925f8b99740
vikaschouhan <presentisgood@gmail.com>
2017-09-02 21:54:59 +0530

    Added support for near months in get_option_chain_nse.py

--- scripts/get_option_chain_nse.py
+++ scripts/get_option_chain_nse.py
@@ -5,15 +5,36 @@ from   bs4 import BeautifulSoup
 from   tabulate import tabulate
 import datetime
 from   dateutil.relativedelta import relativedelta, TH
+import sys
 
-def last_thu():
+##
+# This is pretty hacked up code. Need to clean it up !!
+def last_thu(month_incr=0):
+    if month_incr > 2:
+        print 'month_incr should be between 0 & 2'
+        sys.exit(-1)
+    # endif
     todayte = datetime.datetime.today()
-    cmon = todayte.month
-    t    = None
+    if (todayte.month + month_incr) % 12 == 0:
+        cmon = 12
+        cyear = todayte.year
+    else:
+        cmon    = (todayte.month + month_incr) % 12
+        if cmon < todayte.month:
+            cyear = todayte.year + 1
+        else:
+            cyear = todayte.year
+        # endif
+    # endif
+    #print 'cmon = {}'.format(cmon)
+    t       = None
     
-    for i in range(1, 6):
+    for i in range(1, 24):
         t = todayte + relativedelta(weekday=TH(i))
-        if t.month != cmon:
+        #print 't = {}'.format(t)
+        #print 't.mon = {}'.format(t.month)
+        #print 'cmon = {}'.format(cmon)
+        if datetime.datetime(t.year, t.month, 1) > datetime.datetime(cyear, cmon, 1):
             # since t is exceeded we need last one  which we can get by subtracting -2 since it is already a Thursday.
             t = t + relativedelta(weekday=TH(-2))
             break
@@ -22,13 +43,13 @@ def last_thu():
     return t
 # enddef
 
-def last_thu_str():
+def last_thu_str(month_incr=0):
     mon_l   = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-    exp_day = last_thu()
+    exp_day = last_thu(month_incr)
     return '{}{}{}'.format(exp_day.day, mon_l[exp_day.month-1], exp_day.year)
 # enddef
     
-def option_table(symbol='NIFTY'):
+def option_table(symbol='NIFTY', month_this=0):
     url_this = 'https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTIDX&symbol={}&date={}'
     hdr_this = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -42,8 +63,8 @@ def option_table(symbol='NIFTY'):
                  'Bid Qty', 'Bid Price', 'Ask Price', 'Ask Qty', 'Net Chg', 'LTP', 'IV', 'Volume', 'Chng in OI', 'OI'
                ]  
 
-    act_url  = url_this.format(symbol, last_thu_str())
-    #print 'Fetching from {}'.format(act_url)
+    act_url  = url_this.format(symbol, last_thu_str(month_this))
+    print 'Fetching from {}'.format(act_url)
     req_this = urllib2.Request(act_url, headers=hdr_this)
     page     = urllib2.urlopen(req_this)
     s_this   = page.read()
@@ -66,6 +87,6 @@ def option_table(symbol='NIFTY'):
 # enddef
 
 if __name__ == '__main__':
-    option_tbl_data, option_tbl_hdr = option_table(symbol='NIFTY')
+    option_tbl_data, option_tbl_hdr = option_table(symbol='NIFTY', month_this=0)
     print tabulate(option_tbl_data, headers=option_tbl_hdr)
 # endif
