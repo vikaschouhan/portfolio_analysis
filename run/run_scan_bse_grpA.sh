4da28ca5a122be89d9c125e71d8748518d3a7497
vikaschouhan <presentisgood@gmail.com>
2017-09-14 09:33:53 +0530

    Added a demo run script for scanning bse group A securities.

---
+++ run/run_scan_bse_grpA.sh
@@ -0,0 +1,1 @@
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 60 --res 1W --sfile scripts/db/bse_group_A.csv --down2up --plots_dir ~/candles
