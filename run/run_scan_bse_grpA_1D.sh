db4484eb1c7d8348f9d10e328cb52ed239c3d65e
vikaschouhan <presentisgood@gmail.com>
2017-10-22 12:22:32 +0530

    Added one more run script

---
+++ run/run_scan_bse_grpA_1D.sh
@@ -0,0 +1,1 @@
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 20 --res 1D --sfile scripts/db/bse_group_A.csv --down2up --plots_dir ~/candles_1D
