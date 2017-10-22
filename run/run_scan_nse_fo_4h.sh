b37af3c0074bc498677aab6ee14eb982e956fc06
vikaschouhan <presentisgood@gmail.com>
2017-10-22 15:42:28 +0530

    Added some run scripts and fo_mktlots.csv file.

---
+++ run/run_scan_nse_fo_4h.sh
@@ -0,0 +1,1 @@
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 10 --res 4h --sfile scripts/db/fo_mktlots.csv --plots_dir ~/candles_fo_mktlots_1D
