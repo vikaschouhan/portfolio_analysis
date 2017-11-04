6024850d0946d4d32294b7acd0b48fe4c048bbee
vikaschouhan <presentisgood@gmail.com>
2017-11-04 11:20:42 +0530

    Refactored some code and introduced a new file_type called nse_eqlist_m due to the difference in master eq_list fetched from NSE.

--- scripts/scan_security_list_technical.py
+++ scripts/scan_security_list_technical.py
@@ -173,23 +173,17 @@ def populate_sec_list(sfile):
     sec_list      = []
     l_ctr         = 0
     file_type     = None
+    file_type_l   = ['nse_eqlist', 'nse_eqlist_m', 'bse_eqlist', 'nse_fo_mktlots']
     with open(sfile, 'r') as file_h:
         for l_this in file_h:
             l_ctr = l_ctr + 1
             if l_ctr == 1:
-                if re.match('bse_eqlist', l_this):
-                    file_type = 'bse_eqlist'
-                elif  re.match('nse_eqlist', l_this):
-                    file_type = 'nse_eqlist'
-                elif re.match('nse_fo_mktlots', l_this):
-                    file_type = 'nse_fo_mktlots'
-                else:
-                    file_type = None
-                # endif
-                if file_type:
+                # Get file_type header (1st line)
+                file_type = l_this.replace('\n', '').replace('\r', '').replace(' ', '')
+                if file_type in file_type_l:
                     print Fore.MAGENTA + 'File type seems to be {} !!'.format(file_type) + Fore.RESET
                 else:
-                    print Fore.MAGENTA + 'Unsupported file type. Ensure that first line of csv file specifies file_type !!' + Fore.RESET
+                    print Fore.MAGENTA + 'Unsupported file type {}. Ensure that first line of csv file specifies file_type !!'.format(file_type) + Fore.RESET
                     sys.exit(-1)
                 # endif
                 continue
@@ -209,6 +203,12 @@ def populate_sec_list(sfile):
                 sec_list.append({
                                     'code' : s_arr[2],
                                     'name' : s_arr[0],
+                               })
+            elif file_type == 'nse_eqlist_m':
+                s_arr = l_this.replace('\n', '').split(',')
+                sec_list.append({
+                                    'code' : s_arr[0],
+                                    'name' : s_arr[1],
                                })
             elif file_type == 'nse_fo_mktlots':
                 if re.match('UNDERLYING', l_this) or re.match('Derivatives in', l_this):
