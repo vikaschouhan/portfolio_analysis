3df4de7a4ac5b126b12ac547803350f965470c23
vikaschouhan <presentisgood@gmail.com>
2018-01-07 19:36:55 +0530

    Added one more run script.

---
+++ run/run_scan_bse_grpB_9_14.sh
@@ -0,0 +1,2 @@
+rm -rf ~/candles_bse_group_B
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --ma_plist '9,14,21' --lag 60 --res 1W --sfile scripts/db/bse_group_B.csv --plots_dir ~/candles_bse_group_B --plot_mon
