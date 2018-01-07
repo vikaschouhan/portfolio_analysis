e0760792ba27018496d3f3a9568b81215940d3d8
vikaschouhan <presentisgood@gmail.com>
2018-01-07 13:31:30 +0530

    Added run scripts for dmat security list.

---
+++ run/run_scan_dmat_25_50.sh
@@ -0,0 +1,2 @@
+rm -rf ~/dmat_25_50
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 60 --res 1W --sfile ~/dmat_sym_name_list.csv --plots_dir ~/dmat_25_25 --ma_plist '25,50'
---
+++ run/run_scan_dmat_9_14.sh
@@ -0,0 +1,2 @@
+rm -rf ~/dmat_9_14
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 60 --res 1W --sfile ~/dmat_sym_name_list.csv --plots_dir ~/dmat_9_14 --ma_plist '9,14,21'
