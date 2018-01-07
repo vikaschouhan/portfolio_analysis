bbff021d4bb1573ebf76e1268d83dac30ffea6cc
vikaschouhan <presentisgood@gmail.com>
2018-01-07 13:30:09 +0530

    Added more things to clean in cleanup_junk.sh

--- scripts/cleanup_junk.sh
+++ scripts/cleanup_junk.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 find . -name '*~' -exec rm {} \;
 find . -name '*.pyc' -exec rm {} \;
+find . -name '*.new' -exec rm {} \;
+rm -rf servers/tornado/templates
+rm -rf servers/tornado/output
