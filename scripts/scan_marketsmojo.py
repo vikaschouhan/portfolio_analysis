b75e15abc44a65a3b110ce8221abf962c0a39ec7
vikaschouhan <presentisgood@gmail.com>
2018-02-13 09:40:50 +0530

    Refactoring scan_marketsmojo.py

--- scripts/scan_marketsmojo.py
+++ scripts/scan_marketsmojo.py
@@ -8,50 +8,10 @@
 # (may be company's scrip id or code or name etc). Will add more info to display in future
 
 
-import requests
-from   bs4 import BeautifulSoup
 import argparse
 import pprint
 import sys
-
-def pull_info_from_marketsmojo(scrip):
-    url_search = 'https://www.marketsmojo.com/portfolio-plus/frontendsearch?SearchPhrase={}'
-    url_front  = 'https://www.marketsmojo.com'
-    company_l  = []
-
-    req_this   = requests.get(url_search.format(scrip))
-    if req_this.json() == []:
-        print 'Nothing found for {} !!'.format(scrip)
-        return
-    # endif
-
-    # Go over all of them
-    for item_this in req_this.json():
-        url_page = url_front + item_this[u'url']
-        company  = item_this[u'Company'].replace('<b>', '').replace('</b>', '')
-        bse_code = int(item_this[u'ScriptCode'])
-        nse_code = item_this[u'Symbol']
-        pg_this  = requests.get(url_page)
-
-        # Parse using beautifulsoup
-        html_page = BeautifulSoup(pg_this.text, 'html.parser')
-        ##
-        valuation = html_page.find('div', {'class' : 'valuation cf'}).text.replace('\n', ' ').rstrip(' ').strip(' ')
-        quality   = html_page.find('div', {'class' : 'quality cf'}).text.replace('\n', ' ').rstrip(' ').strip(' ')
-        fin_trend = html_page.find('div', {'class' : 'financials cf'}).text.replace('\n', ' ').rstrip(' ').strip(' ')
-
-        company_l.append({
-                             "name"         : company,
-                             "bsecode"      : bse_code,
-                             "nse_code"     : nse_code,
-                             "valuation"    : valuation,
-                             "quality"      : quality,
-                             "fintrend"     : fin_trend,
-                        })
-    # endfor
-
-    return company_l
-# enddef
+from   modules import invs_core
 
 if __name__ == '__main__':
     parser  = argparse.ArgumentParser()
@@ -63,6 +23,6 @@ if __name__ == '__main__':
         sys.exit(-1)
     # endif
 
-    search_results = pull_info_from_marketsmojo(args.__dict__["scrip"])
+    search_results = invs_core.pull_info_from_marketsmojo(args.__dict__["scrip"])
     pprint.pprint(search_results, indent=4)
 # endif
