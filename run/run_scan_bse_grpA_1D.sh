375cf624e4fe7e690ec4aad3b74ca8f6676cb3d0
vikaschouhan <presentisgood@gmail.com>
2018-02-23 19:27:38 +0530

    Removed lost parameter from one of the run scripts.

--- run/run_scan_bse_grpA_1D.sh
+++ run/run_scan_bse_grpA_1D.sh
@@ -1,2 +1,2 @@
 rm -rf ~/candles_bse_grpA_1D
-python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 20 --res 1D --sfile scripts/db/bse_group_A.csv --down2up --plots_dir ~/candles_bse_grpA_1D
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 20 --res 1D --sfile scripts/db/bse_group_A.csv --plots_dir ~/candles_bse_grpA_1D
