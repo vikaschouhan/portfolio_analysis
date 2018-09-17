7de53f896ed30d5f0203e33297beaa9ce6ce3e20
vikaschouhan <presentisgood@gmail.com>
2018-09-18 00:38:43 +0530

    optargs changes.

--- run/run_scan_dmat_9_14.sh
+++ run/run_scan_dmat_9_14.sh
@@ -1,3 +1,3 @@
 rm -rf ~/dmat_9_14
 #python scripts/scan_dmat.py
-python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 60 --res 1W --sfile ~/dmat_sym_name_list.csv --plots_dir ~/dmat_9_14 --ma_plist '9,14,21'
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 60 --res 1W --sfile ~/dmat_sym_name_list.csv --plots_dir ~/dmat_9_14 --opts ema_fast_period=9,ema_slow_period=14,ema_opt_period=21
