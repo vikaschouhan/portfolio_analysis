d34c05bd8a30f9d8f0da08f81b8e9218ec634840
vikaschouhan <presentisgood@gmail.com>
2018-09-18 00:28:59 +0530

    optargs changes in run_scan_bse_grpA.sh

--- run/run_scan_bse_grpA.sh
+++ run/run_scan_bse_grpA.sh
@@ -1,2 +1,2 @@
 rm -rf ~/candles_bse_group_A_14_21
-python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 60 --res 1W --sfile scripts/db/bse_group_A.csv --plots_dir ~/candles_bse_group_A --ma_plist 14,21
+python scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 60 --res 1W --sfile scripts/db/bse_group_A.csv --plots_dir ~/candles_bse_group_A --opts ema_fast_period=14,ema_slow_period=21,ema_opt_period=9
