d0308e9b0806ce9fa0975beb94952f5a025a7f35
vikaschouhan <presentisgood@gmail.com>
2018-12-25 16:36:11 +0530

    Added option to open option table using default application.

--- scripts/get_option_chain_nse.py
+++ scripts/get_option_chain_nse.py
@@ -3,13 +3,16 @@ from   modules.invs_core import option_table
 from   tabulate import tabulate
 import argparse
 import sys
+import subprocess
 ##
+
 if __name__ == '__main__':
     parser  = argparse.ArgumentParser()
     parser.add_argument('--symbol',      help='OPTION symbol', type=str, default=None)
     parser.add_argument('--month',       help='Month (0 is latest, 1 is next and 2 is next of next.', type=int, default=0)
     parser.add_argument('--instrument',  help='Instrument type (OPTSTK for stock, OPTIDX for indices).', type=int, default=None)
-    parser.add_argument('--outfile',     help='Output file (csv).If none passed table is displayed on console.', type=str, default=None)
+    parser.add_argument('--outfile',     help='''Output file (csv).If none passed table is displayed on console.
+                                               Use --outfile=open to open with default application''', type=str, default=None)
     args    = parser.parse_args()
 
     symbol = args.__dict__['symbol']
@@ -24,7 +27,12 @@ if __name__ == '__main__':
 
     # This is a dataframe
     option_tbl = option_table(symbol=symbol, month=month, instrument=instr)
-    if outf:
+
+    if outf == 'open':
+        tmp_file = '/tmp/____tmp.csv'
+        print('Saving in {}'.format(tmp_file))
+        subprocess.call(['xdg-open', tmp_file])
+    elif outf:
         print('Saving in {}'.format(outf))
         option_tbl.to_csv(outf)
     else:
