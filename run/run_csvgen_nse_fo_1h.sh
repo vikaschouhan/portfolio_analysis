213d09f807d12e840f708c3349464271f498f6cd
Vikas Chouhan <presentisgood@gmail.com>
2019-03-10 00:02:32 +0530

    removed run scripts for csvgen. They can be generated via main run_scan.py using different config files.

--- run/run_csvgen_nse_fo_1h.sh
+++
@@ -1,4 +0,0 @@
-tgt_dir=~/sc_db_github_files/nse_fo_1h_csv
-rm -rf $tgt_dir
-mkdir -p $tgt_dir
-python3 scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --strategy csvgen --res 1h --sfile scripts/db/fo_mktlots.csv --plots_dir $tgt_dir
