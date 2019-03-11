14c63f2492cd98044d9f6d1775ad765c726097ad
Vikas Chouhan <presentisgood@gmail.com>
2019-03-11 08:56:43 +0530

    Removed graph generator run scripts.

--- run/run_graph_bse_grpA.sh
+++
@@ -1,3 +0,0 @@
-tgt_dir=~/sc_db_github_files/weekly_plots_bse_groupA
-rm -rf $tgt_dir
-python3 scripts/scan_security_list_technical.py --sfile scripts/db/bse_group_A.csv --strategy graphgen --res 1W --plots_dir $tgt_dir --plot_period 200 --fig_ratio 2
