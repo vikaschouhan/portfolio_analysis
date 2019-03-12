c0ef92eac498a39e9546a65a49c41b774c92c39c
Vikas Chouhan <presentisgood@gmail.com>
2019-03-12 15:18:36 +0530

    Removed redundant old run scripts for supertrend_rsi strategies.

--- run/run_nse_intraday_supertrend_rsi_long.sh
+++
@@ -1,5 +0,0 @@
-tgt_dir=~/sc_db_github_files/candles_nse_intraday_supertrend_rsi_long
-rm -rf $tgt_dir
-mkdir -p $tgt_dir
-python3 scripts/scan_nse_live_analysis.py --ofile /tmp/__x.csv --key fno_gainers
-python3 scripts/scan_security_list_technical.py --invs scripts/db/investing_dot_com_security_dict.py --lag 10 --res 5m --sfile /tmp/__x.csv --plots_dir $tgt_dir --strategy scanner --strategy_name supertrend_rsi_long
