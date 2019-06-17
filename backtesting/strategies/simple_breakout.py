b337962d48a5f0f5f781c90d58fac4826125ec3e
vikaschouhan <presentisgood@gmail.com>
2019-06-17 09:49:45 +0530

    Fixed some bugs in simple_breakout's implementation.

--- backtesting/strategies/simple_breakout.py
+++ backtesting/strategies/simple_breakout.py
@@ -21,6 +21,7 @@ class SimpleBreakout(StrategyOverride):
         self.order = None
         self.startcash = self.broker.getvalue()
         self.accpoints = 0
+        self.long = None
 
         self.init_tradeid()
         # endif
@@ -32,23 +33,27 @@ class SimpleBreakout(StrategyOverride):
         # endif
 
         if self.data.close > self.data.high[-1]:
-            if self.position: # Close short
+            if self.position and self.long == False: # Close short
                 self.log('CLOSE SHORT , %.2f' % self.data.close[0])
                 self.close(tradeid=self.curtradeid)
+                self.long = None
+            elif self.long == None:
+                self.log('BUY CREATE , %.2f' % self.data.close[0])
+                self.curtradeid = next(self.tradeid)
+                self.buy(size=self.p.stake, tradeid=self.curtradeid)
+                self.long = True
             # endif
-
-            self.log('BUY CREATE , %.2f' % self.data.close[0])
-            self.curtradeid = next(self.tradeid)
-            self.buy(size=self.p.stake, tradeid=self.curtradeid)
         elif self.data.close < self.data.low[-1]:
-            if self.position:  # Close long
+            if self.position and self.long == True:  # Close long
                 self.log('CLOSE LONG , %.2f' % self.data.close[0])
                 self.close(tradeid=self.curtradeid)
+                self.long = None
+            elif self.long == None:
+                self.log('SELL CREATE , %.2f' % self.data.close[0])
+                self.curtradeid = next(self.tradeid)
+                self.sell(size=self.p.stake, tradeid=self.curtradeid)
+                self.long = False
             # endif
-
-            self.log('SELL CREATE , %.2f' % self.data.close[0])
-            self.curtradeid = next(self.tradeid)
-            self.sell(size=self.p.stake, tradeid=self.curtradeid)
         # endif
     # enddef
 # endclass
