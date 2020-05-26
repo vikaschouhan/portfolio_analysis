2547e4161dfc92eb6f654fbb90de1f57aff73e07
vikas <presentisgood@gmail.com>
2020-05-26 23:17:35 +0530

    Added cythonized version of supertrend indicator.

--- backtesting/pandas/core/indicators.py
+++ backtesting/pandas/core/indicators.py
@@ -4,6 +4,7 @@ import talib
 from   memoization import cached
 from   typing import AnyStr
 from   .utils import Cached
+from   cymodules.numpy_ext import *
 
 ######################################
 # Indicators
@@ -28,27 +29,6 @@ def ind_supertrend(high: pd.Series, low: pd.Series, close: pd.Series,
     # Calculate atr
     atr_t  = ind_atr(high, low, close, atr_period)
 
-    # To numpy
-    index  = close.index
-    high   = high.to_numpy()
-    low    = low.to_numpy()
-    close  = close.to_numpy()
-
-    # Base signals
-    avg_t  = (high + low)/2
-    bas_u  = avg_t - atr_multiplier * atr_t
-    bas_l  = avg_t + atr_multiplier * atr_t
-    f_up   = np.zeros_like(close)
-    f_down = np.zeros_like(close)
-    strend = np.zeros_like(close)
-    tpos   = np.zeros_like(close)
-
-    for i, _ in enumerate(close):
-        f_up[i]    = max(bas_u[i], f_up[i-1]) if close[i-1] > f_up[i-1] else bas_u[i]
-        f_down[i]  = min(bas_l[i], f_down[i-1]) if close[i-1] < f_down[i-1] else bas_l[i]
-        tpos[i]    = 1.0 if close[i] > f_down[i-1] else -1.0 if close[i] < f_up[i-1] else tpos[i-1]
-        strend[i]  = f_up[i] if tpos[i] == 1.0 else f_down[i] if tpos[i] == -1.0 else strend[i-1]
-    # endfor
-
-    return pd.Series(strend, index=index).fillna(0.0)
+    return pd.Series(np_ind_supertrend(high.to_numpy(),
+            low.to_numpy(), close.to_numpy(), atr_t.to_numpy(), atr_multiplier), index=high.index).fillna(0.0)
 # endclass
--- cymodules/numpy_ext.pyx
+++ cymodules/numpy_ext.pyx
@@ -12,6 +12,22 @@ cdef double __calc_stop_perc(double p, double l, double x):
     return p * (1 - l*x/100)
 # enddef
 
+cdef double __max(double __x, double __y):
+    if __x > __y:
+        return __x
+    else:
+        return __y
+    # endif
+# enddef
+
+cdef double __min(double __x, double __y):
+    if __x < __y:
+        return __x
+    else:
+        return __y
+    # endif
+# enddef
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
@@ -66,7 +82,7 @@ cdef __np_trail_stop(DTYPE_t[:] pos, DTYPE_t[:] price, double x, DTYPE_t[:] _los
             # endif
         # endif
     # endfor
-# enddef 
+# enddef
 
 cpdef np.ndarray np_trail_fixed_stop(np.ndarray[DTYPE_t, ndim=1] pos, np.ndarray[DTYPE_t, ndim=1] price, x: double):
     cdef np.ndarray[DTYPE_t, ndim=1] _loss = np.zeros_like(price)
@@ -88,4 +104,40 @@ cpdef np.ndarray np_trail_perc_stop(np.ndarray[DTYPE_t, ndim=1] pos, np.ndarray[
     __np_trail_stop(__pos, __price, x,  __loss, 1)
 
     return _loss
+# enddef
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+cdef __np_ind_supertrend(DTYPE_t[:] __high, DTYPE_t[:] __low, DTYPE_t[:] __close, DTYPE_t[:] __atr, double mult, DTYPE_t[:] __strend):
+    cdef DTYPE_t[:] __f_up  = __high.copy()
+    cdef DTYPE_t[:] __f_dn  = __high.copy()
+    cdef DTYPE_t[:] __tpos  = __high.copy()
+    cdef double __avg_t = 0.0
+    cdef double __bas_u = 0.0
+    cdef double __bas_l = 0.0
+
+    for i in range(len(__high)):
+        __avg_t      = (__high[i] + __low[i])/2
+        __bas_u      = __avg_t - mult * __atr[i]
+        __bas_l      = __avg_t + mult * __atr[i]
+        __f_up[i]    = __max(__bas_u, __f_up[i-1]) if __close[i-1] > __f_up[i-1] else __bas_u
+        __f_dn[i]    = __min(__bas_l, __f_dn[i-1]) if __close[i-1] < __f_dn[i-1] else __bas_l
+        __tpos[i]    = 1.0 if __close[i] > __f_dn[i-1] else -1.0 if __close[i] < __f_up[i-1] else __tpos[i-1]
+        __strend[i]  = __f_up[i] if __tpos[i] == 1.0 else __f_dn[i] if __tpos[i] == -1.0 else __strend[i-1]
+    # endfor
+# enddef
+
+cpdef np.ndarray np_ind_supertrend(np.ndarray[DTYPE_t, ndim=1] high, np.ndarray[DTYPE_t, ndim=1] low,
+    np.ndarray[DTYPE_t, ndim=1] close, np.ndarray[DTYPE_t, ndim=1] atr, mult: double):
+    cdef np.ndarray[DTYPE_t, ndim=1] _strend = np.zeros_like(high)
+    cdef DTYPE_t[:] __strend = _strend
+    cdef DTYPE_t[:] __high   = high
+    cdef DTYPE_t[:] __low    = low
+    cdef DTYPE_t[:] __close  = close
+    cdef DTYPE_t[:] __atr    = atr
+
+    __np_ind_supertrend(__high, __low, __close, __atr, mult, __strend)
+
+    return _strend
 # enddef
