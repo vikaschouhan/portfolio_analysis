4afb07959edeb1aedfa5e745ad85ff021cdae473
vikas <presentisgood@gmail.com>
2020-06-05 20:01:20 +0530

    Added 2 more decimals of precision in analyse_top_gainers_loosers.py

--- scripts/analyse_top_gainers_loosers.py
+++ scripts/analyse_top_gainers_loosers.py
@@ -49,8 +49,8 @@ def analyse_rets_data(rets, topn=5, last_candles=10):
         rets_topn = rets_mat.iloc[-indx_t-1].sort_values(ascending=False)[:topn].to_dict()
         rets_botn = rets_mat.iloc[-indx_t-1].sort_values()[:topn].to_dict()
         # add volume
-        rets_topn = __sort_rd({k:precision(v) for k,v in rets_topn.items()})
-        rets_botn = __sort_rd({k:precision(v) for k,v in rets_botn.items()})
+        rets_topn = __sort_rd({k:precision(v, 4) for k,v in rets_topn.items()})
+        rets_botn = __sort_rd({k:precision(v, 4) for k,v in rets_botn.items()})
         # Append
         rets_g.append(rets_topn)
         rets_l.append(rets_botn)
